c9fb6e4ce2c8ad9885490fff11e7a46a9b632594
Azvedo-06 <azinho006@gmail.com>
2025-02-19 11:11:51 -0300

    pratica 04 arrays

---
+++ pratica/pratica04.ts
@@ -0,0 +1,35 @@
+// Array
+
+/* 01 Crie uma função que receba um array de números e retorne a soma de todos os elementos.
+
+function SomaArray(array: number[]): void {
+  let arraySomado = array.reduce(
+    (acumulador, numerosSomados) => acumulador + numerosSomados,
+    0
+  );
+  console.log(`Resultado: ${arraySomado}`);
+}
+
+SomaArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+*/
+
+/* 02 Crie uma função que receba um array de números e retorne um novo array contendo apenas os números pares.
+
+function FiltarPares(array: number[]): void {
+  let arrayFiltradoPares = array.filter(
+    (numeroDoArray) => numeroDoArray % 2 == 0
+  );
+  console.log("Resultado: " + arrayFiltradoPares);
+}
+
+FiltarPares([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+*/
+
+// 03 Crie uma função que receba um array de strings e retorne um novo array com as palavras ordenadas em ordem alfabética.
+
+function OrdenarStrings(array: string[]): void {
+  let arrayOrdenadoAlfabetica = array.sort();
+  console.log("Resultado: " + arrayOrdenadoAlfabetica);
+}
+
+OrdenarStrings(["n", "s", "a", "b"]);
